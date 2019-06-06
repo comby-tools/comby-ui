f6b168fccbaed6f8eb6a90d8ad9dfb1d7a8b7949
Rijnard van Tonder <rvantonder@gmail.com>
2019-06-06 19:08:12 -0400

    add staging configuration file

---
+++ staging/Configuration.elm
@@ -0,0 +1,11 @@
+module Configuration exposing (rewriteServer, thisDomain)
+
+
+thisDomain : String
+thisDomain =
+    "https://staging.comby.live"
+
+
+rewriteServer : String
+rewriteServer =
+    "https://idgaffff.ga:8887"
