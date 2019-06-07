4f65820f42a5945690f6aaf77393d00709d2e7b3
Rijnard van Tonder <rvantonder@gmail.com>
2019-06-06 20:50:43 -0400

    udpate-staging

--- update-staging.sh
+++ update-staging.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 cp staging/* .
-make clean
+make clean &> /dev/null
 make prod
 cp elm.js docs
-cp dark-theme/custom.css
+cp dark-theme/custom.css docs
 git add docs
 git commit -m "docs updated"
 
